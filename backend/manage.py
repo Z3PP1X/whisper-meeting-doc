4812626f91e6589af7de6042e356281f3ad6fd22
Patrick <p.fuchs@hm.edu>
2024-06-06 10:35:05 +0200

    --

---
+++ backend/manage.py
@@ -0,0 +1,3 @@
+##manage.py: A command-line utility that lets you interact with this Django project in various ways. 
+## You can read all the details about manage.py in django-admin and manage.py.##
+## https://docs.djangoproject.com/en/5.0/ref/django-admin/ ##
