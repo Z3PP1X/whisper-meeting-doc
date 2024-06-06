495ba703ce1a8980ae639f1fbc8af5d5aa67536c
Patrick <p.fuchs@hm.edu>
2024-06-06 12:26:01 +0200

    Basic Server Configuration

---
+++ backend/backend_org/ai_meeting_transcript/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AiMeetingTranscriptConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ai_meeting_transcript'
--- backend/backend_org/backend_org/urls.py
+++ backend/backend_org/backend_org/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+
+    path('ai_meeting_transcript/', include('ai_meeting_transcript.urls')),
     path('admin/', admin.site.urls),
 ]
