8f22939408d7a21a30cd0a45b84e3d56775ea653
Patrick <p.fuchs@hm.edu>
2024-06-21 13:44:06 +0200

    added angular

---
+++ frontend/bi_frontend/src/app/audio-recorder/audio-recorder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-recorder',
+  standalone: true,
+  imports: [],
+  templateUrl: './audio-recorder.component.html',
+  styleUrl: './audio-recorder.component.css'
+})
+export class AudioRecorderComponent {
+
+}
